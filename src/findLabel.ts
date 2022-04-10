186cf286567ad569cbabca21d1dfafe93d963657
amberlin10706 <yyyy2248@hotmail.com>
2022-04-10 09:46:38 +0800

    complete

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,19 @@
 import { Permission } from "./types/Permission";
 
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+    let label: string = null
+    permissions
+        .find((p) => {
+            if (p.code === code) {
+                label = p.label;
+                return true;
+            }
+            if (p.children.length > 0) {
+                const ans = findLabel(p.children, code);
+                label = ans;
+                return !!ans;
+            }
+            return false;
+        });
+    return label
 }
--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -4,5 +4,9 @@ import { findLabel } from "../src/findLabel";
 describe("findLabel spec", () => {
     it("should find proper", () => {
         expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
+        expect(findLabel(mockPermissions, "code B-1")).toBe("label B-1")
+        expect(findLabel(mockPermissions, "code C")).toBe("label C")
+        expect(findLabel(mockPermissions, "code D")).toBe(null)
     });
 });
+
