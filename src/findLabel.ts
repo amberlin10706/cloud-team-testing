0ac75c89f29693f2ff767e1afc4b76a2880bf94c
amberlin10706 <yyyy2248@hotmail.com>
2022-10-21 12:03:12 +0800

    some news

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,19 +1,42 @@
 import { Permission } from "./types/Permission";
 
+// 有規則型：得保證children都是繼承parent的code
 export function findLabel(permissions: Permission[], code: string): string {
-    let label: string = null
-    permissions
-        .find((p) => {
-            if (p.code === code) {
-                label = p.label;
-                return true;
+    const findItem = (newPermissions: Permission[] = permissions) => {
+        const item = newPermissions.find((it) => code.startsWith(it.code))
+        if (!item) {
+            return null
+        }
+        if (item.code === code) {
+            return item.label
+        } else {
+            return findItem(item.children)
+        }
+    }
+
+    return findItem()
+}
+
+
+// 土法煉鋼: 每個都跑
+export function findLabelOnEvery(permissions: Permission[], code: string): string {
+    let ans = null;
+    const findItem = (newPermissions: Permission[] = permissions): boolean => {
+        return newPermissions.some((it) => {
+            if (it.code === code) {
+                ans = it.label
+                return true
             }
-            if (p.children.length > 0) {
-                const ans = findLabel(p.children, code);
-                label = ans;
-                return !!ans;
+
+            if (it.children.length > 0) {
+                return findItem(it.children)
             }
-            return false;
-        });
-    return label
+
+            return false
+        })
+    }
+
+    findItem()
+
+    return ans
 }
--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -1,5 +1,5 @@
 import { mockPermissions } from "../dummies/mockPermissions";
-import { findLabel } from "../src/findLabel";
+import { findLabel, findLabelOnEvery } from "../src/findLabel";
 
 describe("findLabel spec", () => {
     it("should find proper", () => {
@@ -7,6 +7,13 @@ describe("findLabel spec", () => {
         expect(findLabel(mockPermissions, "code B-1")).toBe("label B-1")
         expect(findLabel(mockPermissions, "code C")).toBe("label C")
         expect(findLabel(mockPermissions, "code D")).toBe(null)
+    });
+
+    it("should find proper", () => {
+        expect(findLabelOnEvery(mockPermissions, "code B-1-1")).toBe("label B-1-1")
+        expect(findLabelOnEvery(mockPermissions, "code B-1")).toBe("label B-1")
+        expect(findLabelOnEvery(mockPermissions, "code C")).toBe("label C")
+        expect(findLabelOnEvery(mockPermissions, "code D")).toBe(null)
     });
 });
 
